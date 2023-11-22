cd25a468576893fea21368753eeac9942730e96c
kyleung1 <leungkyle2@gmail.com>
2023-11-22 15:15:44 -0800

    palindrome and two sum 2

---
+++ Java/125. Valid Palindrome.java
@@ -0,0 +1,29 @@
+package Java;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] charArr = s.toCharArray();
+        String fixedStr = "";
+
+        for (char c : charArr) {
+            if (Character.isDigit(c) || Character.isLetter(c)) {
+                fixedStr += c;
+            }
+        }
+
+        fixedStr = fixedStr.toLowerCase();
+        int front = 0;
+        int back = fixedStr.length() - 1; 
+
+        while (front <= back) {
+            if (fixedStr.charAt(front) != fixedStr.charAt(back)) {
+                return false;
+            }
+
+            front++;
+            back--;
+        }
+
+        return true;
+    }
+}
---
+++ Java/167. Two Sum II - Input Array Is Sorted.java
@@ -0,0 +1,21 @@
+package Java;
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int front = 0;
+        int back = numbers.length - 1;
+
+        while (front <= back) {
+            int sum = numbers[front] + numbers[back];
+            if (sum < target) {
+                front++;
+            } else if (sum > target) {
+                back--;
+            } else {
+                return new int[] {front + 1, back + 1};
+            }
+        }
+
+        return new int[] {};
+    }
+}
